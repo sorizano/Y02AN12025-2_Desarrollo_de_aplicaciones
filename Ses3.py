719aef85982b464a4086d2a7eda4e9be7eeb7ff7
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2025-02-25 21:59:18 -0500

    creando menú

--- Ses3.py
+++ Ses3.py
@@ -9,15 +9,23 @@ def clasificar_puntaje(puntaje):
     else:
         return "Necesita Mejorar"
     
+# Menú Lateral
+st.sidebar.title("Menú de navegación")
+opcion = st.sidebar.selectbox("Seleccione una opción", ["Inicio", "Clasificación de Puntajes"])
 
-# Interfaz en Streamlit
-st.title("Clasificación de Puntajes")
-st.write("Ingese un puntaje y el sistema lo clasificará.")
+# Sección: Inicio
+if opcion == "Inicio":
+    st.title("Bienvenid@ a la aplicación") 
 
-# Entrada de usuario
-puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max_value=100, step=1)
+# Sección: Clasificación de Puntajes
+elif opcion == "Clasificación de Puntajes"
+    st.title("Clasificación de Puntajes")
+    st.write("Ingese un puntaje y el sistema lo clasificará.")
 
-# Botón para clasificar
-if st.button("Clasificar"):
-    resultado = clasificar_puntaje(puntaje)
-    st.success(f"El puntaje {puntaje} es clasificado como: {resultado}")
+    # Entrada de usuario
+    puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max_value=100, step=1)
+
+    # Botón para clasificar
+    if st.button("Clasificar"):
+        resultado = clasificar_puntaje(puntaje)
+        st.success(f"El puntaje {puntaje} es clasificado como: {resultado}")
