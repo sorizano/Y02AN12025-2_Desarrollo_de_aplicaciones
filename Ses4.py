844d3278f8b2be5e733cc07c64f1f014479400aa
Sergio Orizano Salvador <svorizano@gmail.com>
2025-03-04 21:36:00 -0500

    Creando el archivo Ses4

---
+++ Ses4.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Estructuras de Control Repetitivas")
+
+#Entrada del usuario para límite de bucle
+limite = st.number_input("Ingrese un número límite para los bucles: ", min_value=1, step=1)
+
+st.subheader("Bucle FOR")
+
+for i in range(1, limite + 1):
+    st.write(f"Iteración {i} con for")
